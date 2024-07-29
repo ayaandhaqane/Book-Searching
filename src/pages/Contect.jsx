be9097465aa2453eadec8959201a75c35c5a52e0
ayaandhaqane <148534224+ayaandhaqane@users.noreply.github.com>
2024-07-29 19:00:05 +0300

    commit

--- src/pages/Contect.jsx
+++ src/pages/Contect.jsx
@@ -14,11 +14,11 @@ function Contact() {
         </section>
       </Suspense>
 
-      <section className="bg-gradient-to-b from-[#101212] to-[#08201D] min-h-screen flex items-center justify-center">
+      <section className="bg-[ #f3f2fc] min-h-screen flex items-center justify-center">
         <div className="container px-6 py-3 mx-auto  rounded-2xl">
           <div className="lg:flex lg:items-center lg:-mx-6">
             <div className="lg:w-1/3 lg:mx-6">
-              <h1 className="text-2xl font-semibold text-white capitalize text-white lg:text-3xl">
+              <h1 className="text-2xl font-semibold text-white capitalize text-[#4B0082] lg:text-3xl">
                 Contact us for more info
               </h1>
 
@@ -28,7 +28,7 @@ function Contact() {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span className="mx-2 text-white truncate w-72 text-white">
+                  <span className="mx-2 text-[#4B0082] truncate w-72 text-white">
                     Mugadisho
                     St.Hodan
                   </span>
@@ -37,13 +37,13 @@ function Contact() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <span className="mx-2 truncate w-72 text-white">(252) 615-107933</span>
+                  <span className="mx-2 truncate w-72 text-[#4B0082]">(252) 615-107933</span>
                 </p>
                 <p className="flex items-start -mx-2">
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
-                  <span className="mx-2 text-gray-700 truncate w-72 text-white">acb@example.com</span>
+                  <span className="mx-2 text-gray-700 truncate w-72 text-[#4B0082]">acb@example.com</span>
                 </p>
               </div>
 
@@ -92,7 +92,7 @@ function Contact() {
                     <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-blue-950 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
                   </div>
 
-                  <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                  <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-black capitalize transition-colors duration-300 transform bg-white rounded-md hover:bg-[#4B0082] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                     get in touch
                   </button>
                 </form>
